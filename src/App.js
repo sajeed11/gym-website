e09cb3d9f0dbd54f7b2c1a1ad574b08a8f0383c9
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-09-29 18:06:01 +0100

    Second Day

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 };

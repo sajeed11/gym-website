c33a15d5b60194d8aa41ddc047f20b184054bdbf
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-10-01 23:14:46 +0100

    Sixth Day

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png";
+import { Block } from "@mui/icons-material";
+
 const HeroBanner = () => {
   return (
     <Box
@@ -7,22 +11,41 @@ const HeroBanner = () => {
       position="relative"
       p="20px"
     >
-      <Typography color="FF2625" fontWeight="600" fontSize="26px">
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
       <Typography
         fontWeight={700}
         sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mt="30px"
+        mb="23px"
       >
         Sweat, Smile <br /> and Reapeat
       </Typography>
-      <Typography fontSize="22px" lineHeight="35px" mb={2}>
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out the most effective exercises.
       </Typography>
 
-      <Button variant="contained" color="error">
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#FF2625", padding: "10px" }}
+      >
         Explore Exercises
       </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" },
+        }}
+        fontSize="200px"
+      >
+        Exercises
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
 };
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography, Button, TextField } from "@mui/material";
+
+import { exercisesOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState("");
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back",
+        exercisesOptions
+      );
+      console.log(exercisesData);
+    }
+  };
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "30px" },
+        }}
+        textAlign="center"
+        mb="50px"
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          value={search}
+          onChange={(e) => {
+            e.target.value.toLowerCase();
+          }}
+          placeholder="Search Exersices"
+          type="text"
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#FFF",
+            borderRadius: "40px",
+          }}
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#FFF",
+            textTransform: "none",
+            width: { lg: "175px", xs: "100px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
